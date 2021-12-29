dd4b9511559d33addf18d041417ce30ddfdfadb2
rekha2110 <rekhalingappa321@gmail.com>
2021-12-29 23:14:34 +0530

    added 1 project

---
+++ File Handling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
